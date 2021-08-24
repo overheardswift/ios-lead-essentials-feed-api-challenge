1ee702286e1d0c2e0bfb2759e1738fb9bd446213
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-24 20:17:23 +0700

    Create `FeedImagesMapper` class helper

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,49 +24,56 @@ public final class RemoteFeedLoader: FeedLoader {
 
 			switch result {
 			case let .success((data, response)):
-				if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
-					completion(.success(root.feedImages))
-				} else {
-					completion(.failure(Error.invalidData))
-				}
+				completion(FeedImagesMapper.map(data, response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
+	
+	private final class FeedImagesMapper {
+		
+		private struct Root: Decodable {
+			private let images: [Image]
 
-	private struct Root: Decodable {
-		private let images: [Image]
+			var feedImages: [FeedImage] {
+				return images.map { $0.image }
+			}
 
-		var feedImages: [FeedImage] {
-			return images.map { $0.image }
+			enum CodingKeys: String, CodingKey {
+				case images = "items"
+			}
 		}
 
-		enum CodingKeys: String, CodingKey {
-			case images = "items"
+		private struct Image: Decodable {
+			private let id: UUID
+			private let description: String?
+			private let location: String?
+			private let url: URL
+
+			var image: FeedImage {
+				return FeedImage(
+					id: id,
+					description: description,
+					location: location,
+					url: url
+				)
+			}
+
+			enum CodingKeys: String, CodingKey {
+				case id = "image_id"
+				case description = "image_desc"
+				case location = "image_loc"
+				case url = "image_url"
+			}
+		}
+		
+		static func map(_ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
+			guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+				return .failure(Error.invalidData)
+			}
+			return .success(root.feedImages)
 		}
 	}
-
-	private struct Image: Decodable {
-		private let id: UUID
-		private let description: String?
-		private let location: String?
-		private let url: URL
-
-		var image: FeedImage {
-			return FeedImage(
-				id: id,
-				description: description,
-				location: location,
-				url: url
-			)
-		}
-
-		enum CodingKeys: String, CodingKey {
-			case id = "image_id"
-			case description = "image_desc"
-			case location = "image_loc"
-			case url = "image_url"
-		}
-	}
+	
 }
