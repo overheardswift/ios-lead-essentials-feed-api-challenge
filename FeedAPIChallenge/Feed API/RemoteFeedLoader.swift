6359940594425901f83d01418060fff3660885a1
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-24 17:35:24 +0700

    Delivers  `.invalidData` error on 200 HTTP response with invalid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,9 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success(_, response):
-				if response.statusCode != 200 {
-					completion(.failure(Error.invalidData))
-				}
+				completion(.failure(Error.invalidData))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
